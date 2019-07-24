e239fd975291ce783cdc4ff5cc358c424ab9415c
Ashish Tajane <ashishtajane@gmail.com>
2019-07-25 01:34:54 +0530

    Add user certificates retrieval endpoint in client
    Follow all conventions as per existing classes in the gem

--- lib/avatax/api/customers.rb
+++ lib/avatax/api/customers.rb
@@ -19,6 +19,23 @@ module Avatax
         resp = connection.post "api/v2/companies/#{company_code}/customers", args
         handle_response(resp)
       end
+
+      ##
+      # List certificates for a customer within a company.
+      # @see https://developer.avalara.com/api-reference/avatax/rest/v2/methods/Customers/ListCertificatesForCustomer/
+      #
+      # @param company_code [String] The company_code in avatax.
+      # @param customer_code [String] The customer_code in avatax.
+      # @param args [Hash] Arguments for avatax.
+      # @return [Avatax::Response]
+      #
+      def certificates(company_code, customer_code, args = {})
+        raise ArgumentError, 'company_code is required' if company_code.blank?
+        raise ArgumentError, 'customer_code is required' if customer_code.blank?
+
+        resp = connection.get "api/v2/companies/#{company_code}/customers/#{customer_code}/certificates", args
+        handle_response(resp)
+      end
     end
   end
 end
