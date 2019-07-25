11bf76177d3dfbb1fc819ebe6450d6a9b2610302
Ashish Tajane <ashishtajane@gmail.com>
2019-07-25 17:05:00 +0530

    Add endpoint to link bill-to and ship-to customers

--- lib/avatax/api/customers.rb
+++ lib/avatax/api/customers.rb
@@ -36,6 +36,23 @@ module Avatax
         resp = connection.get "api/v2/companies/#{company_code}/customers/#{customer_code}/certificates", args
         handle_response(resp)
       end
+
+      ##
+      # Link bill-to customer to a list of ship-to customers
+      # @see https://developer.avalara.com/api-reference/avatax/rest/v2/methods/Customers/LinkShipToCustomersToBillCustomer/
+      #
+      # @param company_code [String] The company_code in avatax.
+      # @param customer_code [String] The customer_code in avatax.
+      # @param args [Hash] Arguments for avatax.
+      # @return [Avatax::Response]
+      #
+      def link_ship_to(company_code, customer_code, args = {})
+        raise ArgumentError, 'company_code is required' if company_code.blank?
+        raise ArgumentError, 'customer_code is required' if customer_code.blank?
+
+        resp = connection.post "api/v2/companies/#{company_code}/customers/billto/#{customer_code}/shipto/link", args
+        handle_response(resp)
+      end
     end
   end
 end
